4e80f6ff4644d1342629c8941f6fad92a051b30e
Adaseja <lisicka.adrianna@gmail.com>
2023-09-26 16:11:49 +0200

    creating a get LatestPosts function

---
+++ src/components/Post.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Post = ({ post }) => {
+    return (
+        <div className="post">
+
+</div>
+  );
+};
+
+export default Post;
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Post from './components/Post';
 
-const Home = () => {
+
+const [posts, setPosts] = useState([]);
+
+const getLatestPosts = () => {
+    fetch('http://akademia108.pl/api/social-app/post/latest', {
+      method: 'GET' });
+
+      useEffect(() => {
+        getLatestPosts();
+      }, []);
+
     return (
         <div>
             <h2>Home</h2>
