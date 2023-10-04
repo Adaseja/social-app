fa9d1c40c8195310f26ec4c8197f523044cd89d9
Adaseja <lisicka.adrianna@gmail.com>
2023-10-04 12:29:36 +0200

    correcting react component

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,10 +1,14 @@
+
 import React from 'react';
 
-const Post = ({ post }) => {
-    return (
-        <div className="post">
-
-</div>
+const Post = (props) => {
+  return (
+    <div className="post">
+      <div className="avatar">
+        <img src={props.post.user.avatar_url} alt={props.post.user.username} />
+      </div>
+     
+    </div>
   );
 };
 
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,22 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import Post from './components/Post';
 
+const Home = (props) => {
+  const [posts, setPosts] = useState([]);
 
-const [posts, setPosts] = useState([]);
-
-const getLatestPosts = () => {
+  const getLatestPosts = () => {
     fetch('http://akademia108.pl/api/social-app/post/latest', {
-      method: 'GET' });
+      method: 'GET'
+    })
+      .then((req) => req.json()) // Parse the response directly to JSON
+      .then((reqData) => {
+        setPosts(reqData);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
-      useEffect(() => {
-        getLatestPosts();
-      }, []);
+  useEffect(() => {
+    getLatestPosts();
+  }, []);
 
-    return (
-        <div>
-            <h2>Home</h2>
-        </div>
-    );
+  return (
+    <div>
+      {posts.map((post) => (
+        <Post  />
+      ))}
+    </div>
+  );
 }
 
 export default Home;
